e0d677fc759e5025d8558277c3fdffd6fa6dde81
ARTHUR MARTINS <arthurmvo@gmail.com>
2023-08-24 13:10:42 -0300

    Updated USA tax and button hidden if no conversion rate available

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-let source_currency = "";
-let target_currency = "";
-let curr_list = [];
-
+const temp_msg = document.getElementById("temp-msg");
 const msg1 = document.getElementById("msg1");
 const msg3 = document.getElementById("msg3");
 const msg_resultado = document.getElementById("msg_resultado");
@@ -23,7 +20,8 @@ const getExRate = async () => {
   let buyValue = doc.querySelector(".buy-value").textContent;
 //   console.log(buyValue);
   msg3.textContent = `A cotação atual do dolar MEP é ${buyValue}`;
-
+  temp_msg.hidden = true;
+  convert_btn.hidden = false;
   return buyValue;
 };
 
@@ -45,11 +43,11 @@ const analyze = async () => {
   msg_resultado3.hidden = false;
 
   if (dolar/preco_convertido > 1){
-    msg_resultado.textContent = `O preço na Argentina é USD${preco_convertido.toFixed(2)} e nos EUA está ${dolar.toFixed(2)}.`;
+    msg_resultado.textContent = `O preço na Argentina é $${preco_convertido.toFixed(2)} e nos EUA está $${dolar.toFixed(2)} (com a taxa).`;
     msg_resultado2.textContent = `Na Argentina está ${Math.round(100*(dolar/preco_convertido)- 100)}% mais barato`
     msg_resultado3.textContent = `Vale mais a pena comprar na Argentina`;
   } else {
-    msg_resultado.textContent = `O preço na Argentina é USD${preco_convertido.toFixed(2)} e nos EUA está ${dolar.toFixed(2)}.`;
+    msg_resultado.textContent = `O preço na Argentina é $${preco_convertido.toFixed(2)} e nos EUA está $${dolar.toFixed(2)} (com a taxa).`;
     msg_resultado2.textContent = `Nos EUA está ${Math.round((1- dolar/preco_convertido)*100)}% mais barato`
     msg_resultado3.textContent = `Vale mais a pena comprar nos EUA`;
   }
